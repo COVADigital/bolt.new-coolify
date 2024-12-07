10c98fd7056a617da1ba47cca55616623769242b
COVA Diigital <joseph.g.bain@gmail.com>
2024-12-07 00:35:55 -0500

    Normalize line endings and update configuration

--- app/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-interface Window {
-  showDirectoryPicker(): Promise<FileSystemDirectoryHandle>;
-  webkitSpeechRecognition: typeof SpeechRecognition;
-  SpeechRecognition: typeof SpeechRecognition;
-}
